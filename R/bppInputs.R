f468713bdf87836c2a444ba6beb7c25a719c1c3f
Dan MacGuigan <daniel.macguigan@yale.edu>
2019-12-24 11:53:16 -0500

    added plotting by priors

--- R/bppInputs.R
+++ R/bppInputs.R
@@ -392,7 +392,7 @@ plotByPrior <- function(gdiDat, wd, nreps, priors, plotWidth, plotHeight) {
 
   for (i in 1:length(allGDIList_byPrior)) {
     dat = as.data.frame(allGDIList_byPrior[[i]])
-    pdf(file=paste("priors-tau", prior_df[i,1], prior_df[i,2], "theta", prior_df[i,3], prior_df[i,4], sep=""), width=plotWidth, height=plotHeight)
+    pdf(file=paste("priors-tau", prior_df[i,1], prior_df[i,2], "theta", prior_df[i,3], prior_df[i,4], "_boxplot.pdf", sep=""), width=plotWidth, height=plotHeight)
     p <- ggplot(data = melt(dat), aes(y=value, x=variable)) +
       ylim(c(0,1)) +
       geom_hline(yintercept = 0.2, lty=2)+
@@ -416,7 +416,7 @@ plotByPrior <- function(gdiDat, wd, nreps, priors, plotWidth, plotHeight) {
     dat_mean$ci_high <- apply(as.data.frame(allGDIList_byPrior[[i]]), 2, function(x) ci(x, method="HDI", ci=0.95)$CI_high)
     colnames(dat_mean) <- c("species", "mean", "ci_low", "ci_high")
         
-    pdf(file=paste("priors-tau", prior_df[i,1], prior_df[i,2], "theta", prior_df[i,3], prior_df[i,4], sep=""), width=plotWidth, height=plotHeight)
+    pdf(file=paste("priors-tau", prior_df[i,1], prior_df[i,2], "theta", prior_df[i,3], prior_df[i,4], "_means.pdf", sep=""), width=plotWidth, height=plotHeight)
     p <- ggplot(data=dat_mean, aes(x=species, y=mean)) +
       ylim(c(0,1)) +
       geom_hline(yintercept = 0.2, lty=2) +
